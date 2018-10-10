84dbfdeb13e480a229ebccc3782b7c65cd7b49a6
hina ghous <43152766+hinaghous@users.noreply.github.com>
2018-10-10 21:29:16 +0200

    Create User.java

---
+++ core/src/main/java/iit/uni/miskolc/hu/softwaretesting/model/User.java
@@ -0,0 +1,11 @@
+package iit.uni.miskolc.hu.softwaretesting.model;
+
+public class User {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String username;
+    private String password;
+
+}
