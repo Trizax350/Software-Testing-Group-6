a31794673297fc8f9c9173cd91a312789ca0688c
prenoo <prenomail@gmail.com>
2018-10-14 17:56:46 +0200

    Created the (so far empty) interfaces and classes for custom exceptions

---
+++ core/src/main/java/iit/uni/miskolc/hu/softwaretesting/service/RequestTypeAlreadyExist.java
@@ -0,0 +1,9 @@
+//this will be thrown in the Administrator class when a request type already exists
+package iit.uni.miskolc.hu.softwaretesting.service;
+
+public class RequestTypeAlreadyExist {
+    public RequestTypeAlreadyExist(String errorMessage) {
+        super(errorMessage);
+    }
+
+}
