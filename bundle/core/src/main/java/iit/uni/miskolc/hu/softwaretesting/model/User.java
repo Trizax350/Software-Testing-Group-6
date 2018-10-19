e1154037cfc8356c79deb6665a3775bc293d8f35
Hajnal Róbert Dávid <trizax350@gmail.com>
2018-10-19 08:32:41 +0200

    Fixed merge

--- bundle/core/src/main/java/iit/uni/miskolc/hu/softwaretesting/model/User.java
+++ bundle/core/src/main/java/iit/uni/miskolc/hu/softwaretesting/model/User.java
@@ -1,60 +1,37 @@
+package iit.uni.miskolc.hu.softwaretesting.model;
 
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String username;
+    private String password;
 
-public class User{
-	
-	    private Integer id;
-	    private String name;
-	    private String email;
-	    private String username;
-	    private String password;
-	    private UserType userType;
-		
-		public User(Integer id, String name, String email, String username, String password, UserType userType) {
-			super();
-			this.id = id;
-			this.name = name;
-			this.email = email;
-			this.username = username;
-			this.password = password;
-			this.userType = userType;
-		}
-		
-		public Integer getId() {
-			return id;
-		}
-		public void setId(Integer id) {
-			this.id = id;
-		}
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public String getEmail() {
-			return email;
-		}
-		public void setEmail(String email) {
-			this.email = email;
-		}
-		public String getUsername() {
-			return username;
-		}
-		public void setUsername(String username) {
-			this.username = username;
-		}
-		public String getPassword() {
-			return password;
-		}
-		public void setPassword(String password) {
-			this.password = password;
-		}
-	    
-		public UserType getUserType() {
-			return userType;
-		}
+    public User(int id, String name, String email, String username, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
 
-		public void setUserType(UserType userType) {
-			this.userType = userType;
-		}
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
--- bundle/core/src/main/java/iit/uni/miskolc/hu/softwaretesting/model/UserType.java
+++
@@ -1,5 +0,0 @@
-package iit.uni.miskolc.hu.softwaretesting.model;
-
-public enum UserType {
-Student,Teacher,Administrator;
-}
