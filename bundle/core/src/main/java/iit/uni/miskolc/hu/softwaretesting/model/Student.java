463dc857f110b8a9c586c6cf7d3a8e162aec8bad
prenoo <prenomail@gmail.com>
2018-10-23 15:00:56 +0200

    Removed unnecessary 'public' keywords from the method names in interfaces. Fixed how the students' statuses work in the Student class.

--- bundle/core/src/main/java/iit/uni/miskolc/hu/softwaretesting/model/Student.java
+++ bundle/core/src/main/java/iit/uni/miskolc/hu/softwaretesting/model/Student.java
@@ -1,19 +1,22 @@
 package iit.uni.miskolc.hu.softwaretesting.model;
+import java.util.Collection;
 
 public class Student extends User {
 
 
     //students can be active or passive
     private enum Status {
-        ACTIVE, PASSIVE;
+        ACTIVE, PASSIVE
     }
 
-        public Student(int id, String name, String email, String username, String password, Status status) {
+    private Collection<Status> status;
+
+    public Student(int id, String name, String email, String username, String password, Collection<Status> status) {
         super(id, name, email, username, password);
         this.status = status;
     }
 
-    public getStatus() {
+    public Collection<Status> getStatus() {
         return status;
     }
 }
--- bundle/core/src/main/java/iit/uni/miskolc/hu/softwaretesting/service/StudentInterface.java
+++ bundle/core/src/main/java/iit/uni/miskolc/hu/softwaretesting/service/StudentInterface.java
@@ -6,7 +6,7 @@ import iit.uni.miskolc.hu.softwaretesting.model.Request;
 
 public interface StudentInterface extends UserInterface {
 
-    public void addCourse(Course course);
-    public void deleteCourse(Course course);
-    public void sendRequest (Request request);
+    void addCourse(Course course);
+    void deleteCourse(Course course);
+    void sendRequest (Request request);
 }
