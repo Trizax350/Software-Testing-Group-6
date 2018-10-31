68ad3951b6ee63b2d9da2a196bc77b06ffe9f0d4
Sólyom Tamás <43153441+solyom2@users.noreply.github.com>
2018-10-31 15:13:08 +0100

    Methods are added to ManageComplains and Request interfaces

--- bundle/core/src/main/java/iit/uni/miskolc/hu/softwaretesting/service/ManageComplainsInterface.java
+++ bundle/core/src/main/java/iit/uni/miskolc/hu/softwaretesting/service/ManageComplainsInterface.java
@@ -1,8 +1,17 @@
 package iit.uni.miskolc.hu.softwaretesting.service;
 
 
+import iit.uni.miskolc.hu.softwaretesting.model.Complain;
+
+import java.util.Collection;
+
 public interface ManageComplainsInterface {
 
-
+    void makeComplain(Complain complain);
+    void updateComplain(Complain complain);
+    void deleteComplain(Complain complain);
+    Collection<Complain> getAllComplain();
+    Collection<Complain> getAllComplainByRequest(int req_id);
+    Collection<Complain> findComplainById(int id);
 
 }
--- bundle/core/src/main/java/iit/uni/miskolc/hu/softwaretesting/service/ManageRequestsInterface.java
+++ bundle/core/src/main/java/iit/uni/miskolc/hu/softwaretesting/service/ManageRequestsInterface.java
@@ -6,8 +6,15 @@ import java.util.Collection;
 
 public interface ManageRequestsInterface {
 
+    void sendRequest(Request request);
+    void updateRequest(Request request);
+    void deleteRequest(Request request);
     Collection<Request> getAllRequest();
-    void sendRequest (Request request);
-
+    Collection<Request> getAllRequestByType(String type);
+    Collection<Request> getAllRequestByStatus(String status);   //return open or closed requests
+    Collection<Request> getAllRequestWithComplain();
+    Collection<Request> getAllRequestByCourse(String reference_to_the_Course);
+    Collection<Request> getAllRequestByUser(int referenceToUser);
+    Collection<Request> findRequestById(int id);
 
 }
