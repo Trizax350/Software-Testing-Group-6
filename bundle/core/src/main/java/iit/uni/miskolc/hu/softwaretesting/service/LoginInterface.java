af1de427b3684eef3abe937b9ee3b1914733fd79
hina ghous <43152766+hinaghous@users.noreply.github.com>
2018-11-01 18:30:03 +0100

    Update LoginInterface.java
    there should be an  changePasword option so i have created method for changePasswod and one thing more i think we have to pass User object in each method to get the details.

--- bundle/core/src/main/java/iit/uni/miskolc/hu/softwaretesting/service/LoginInterface.java
+++ bundle/core/src/main/java/iit/uni/miskolc/hu/softwaretesting/service/LoginInterface.java
@@ -2,9 +2,9 @@ package iit.uni.miskolc.hu.softwaretesting.service;
 
 public interface LoginInterface {
 
-    public boolean successfulLogin();
-    public String lostPassword();
-    public boolean goodPassword();
-    public boolean authenticate(String username, String password);
-
+    public boolean successfulLogin(User user);
+    public String lostPassword(User user);
+    public boolean goodPassword(User user);
+    public boolean authenticate(User user);
+    public boolean changePassword(User user);
 }
