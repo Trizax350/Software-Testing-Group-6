29c665567a29ea8e6a30d75ea16480c995bb8c47
hina ghous <43152766+hinaghous@users.noreply.github.com>
2018-11-01 18:40:41 +0100

    Update Teacher.java

--- bundle/core/src/main/java/iit/uni/miskolc/hu/softwaretesting/model/Teacher.java
+++ bundle/core/src/main/java/iit/uni/miskolc/hu/softwaretesting/model/Teacher.java
@@ -1,7 +1,7 @@
 package iit.uni.miskolc.hu.softwaretesting.model;
 
 public class Teacher extends User {
-    private String department; //intezet/tanszek
+    private String department; //Institte
     public Teacher(int id, String name, String email, String username, String password, String department) {
         super(id, name, email, username, password);
         this.department = department;
