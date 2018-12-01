90bec611fa5a46f6d477a1726b03fdcd16fbe89c
margar00 <margaretapapp@gmail.com>
2018-12-01 20:59:05 +0100

    Complain service test

--- bundle/service/src/test/java/iit/uni/miskolc/hu/softwaretesting/ManageComplainsInterfaceImplTest.java
+++ bundle/service/src/test/java/iit/uni/miskolc/hu/softwaretesting/ManageComplainsInterfaceImplTest.java
@@ -9,9 +9,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doReturn;
 
 public class ManageComplainsInterfaceImplTest {
 
@@ -31,42 +33,68 @@ public class ManageComplainsInterfaceImplTest {
     }
 
     @Test
-    public void testMakeComplain(){
-
+    public void testMakeComplain() throws Exception{
+        Complain complain = new Complain(11, "leiras", 11);
+        complainManager.makeComplain(complain);
     }
 
     @Test
-    public void testUpdateComplain(){
+    public void testUpdateComplain()throws Exception{
+        Complain complain1 = new Complain(11, "leiras1", 1);
+        Complain complain2 = new Complain(11, "leiras2", 1);
 
+        complainManager.makeComplain(complain1);
+        complainManager.updateComplain(complain2);
     }
 
     @Test
-    public void testDeleteComplain(){
+    public void testDeleteComplain() throws Exception{
+        Complain complain= new Complain(11, "leiras", 1);
 
+        complainManager.makeComplain(complain);
+        complainManager.deleteComplain(complain);
     }
 
     @Test
-    public void testAcceptComplain(){
+    public void testAcceptComplain() throws Exception{
+        Complain complain= new Complain(11, "leiras", 1);
 
+        complainManager.makeComplain(complain);
+        complainManager.acceptComplain(complain);
     }
 
     @Test
-    public void testRejectComplain(){
+    public void testRejectComplain() throws Exception{
+        Complain complain= new Complain(11, "leiras", 1);
 
+        complainManager.makeComplain(complain);
+        complainManager.rejectComplain(complain);
     }
 
     @Test
-    public void testGetAllComplain(){
+    public void testGetAllComplain() throws Exception{
+        ArrayList<Complain> complains = new ArrayList<>();
+        complains.add(new Complain(11, "leiras", 1));
 
+        doReturn(complains).when(complainDAOMock).getAllComplain();
+        assertEquals(complains, complainManager.getAllComplain());
     }
 
     @Test
-    public void testGetAllComplainByRequest(){
+    public void testGetAllComplainByRequest() throws Exception{
+        ArrayList<Complain> complains = new ArrayList<>();
+        complains.add(new Complain(11, "leiras", 1));
 
+        doReturn(complains).when(complainDAOMock).getAllComplainByRequest(1);
+        assertEquals(complains, complainManager.getAllComplainByRequest(1));
     }
 
     @Test
-    public void testFindComplainById(){
+    public void testFindComplainById() throws Exception{
+        ArrayList<Complain> complains = new ArrayList<>();
+        complains.add(new Complain(11, "leiras", 1));
 
+        doReturn(complains).when(complainDAOMock).findComplainById(1);
+        assertEquals(complains, complainManager.findComplainById(1));
     }
 }
