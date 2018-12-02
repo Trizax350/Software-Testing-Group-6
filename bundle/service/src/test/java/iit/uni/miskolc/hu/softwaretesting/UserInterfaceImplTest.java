d9920d3cba4b7c9ee3bd5182cee2c7f668d91731
prenoo <prenomail@gmail.com>
2018-12-02 20:37:51 +0100

    UserInterface tests

--- bundle/service/src/test/java/iit/uni/miskolc/hu/softwaretesting/UserInterfaceImplTest.java
+++ bundle/service/src/test/java/iit/uni/miskolc/hu/softwaretesting/UserInterfaceImplTest.java
@@ -1,5 +1,6 @@
 package iit.uni.miskolc.hu.softwaretesting;
 
+import iit.uni.miskolc.hu.softwaretesting.model.User;
 import iit.uni.miskolc.hu.softwaretesting.service.UserInterfaceImpl;
 import iit.uni.miskolc.hu.softwaretesting.dao.UserDAO;
 import org.junit.Before;
@@ -28,22 +29,33 @@ public class UserInterfaceImplTest {
     }
 
     @Test
-    public void testAddUser(){
+    public void testAddUser() throws Exception {
+        User user = new User(11, "tesztuser", "tesztuser@gmail.com", "user1", "password1");
+        userManager.addUser(user);
+    }
+
+    @Test
+    public void testUpdateUser() throws Exception{
+        User user = new User(11, "tesztuser", "tesztuser@gmail.com", "user1", "password1");
+        User user2 = new User(11, "tesztuser", "tesztuser@gmail.com", "user1", "password222");
+
+        userManager.addUser(user);
+        userManager.updateUser(user2);
 
     }
 
     @Test
-    public void testUpdateUser(){
+    public void testDeleteUser() throws Exception{
+        User user = new User(13, "tesztuser", "tesztuser@gmail.com", "user1", "password1");
 
-    }
-
-    @Test
-    public void testDeleteUser(){
+        userManager.addUser(user);
+        userManager.deleteUser(user);
 
     }
 
     @Test
     public void testGetAllUser(){
+
 
     }
 
